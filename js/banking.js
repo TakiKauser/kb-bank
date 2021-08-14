bbff10b8f6ab96752487be0cd38e5e297b4945d5
TakiKauser <takikauserbadhon@gmail.com>
2021-08-15 04:07:44 +0600

    deposit and balance total updated

--- js/banking.js
+++ js/banking.js
@@ -1,11 +1,30 @@
 document.getElementById("deposit-btn").addEventListener("click", function(){
     // get dipositted amount
     const depositAmountInput = document.getElementById("deposit-amount-input");
-    const depositAmount = depositAmountInput.value;
+    const depositAmountText = depositAmountInput.value;
+    const depositAmount = parseFloat(depositAmountText);
     // console.log(depositAmount);
 
+    // update total diposit 
     const totalDeposit = document.getElementById("total-deposit");
-    totalDeposit.innerText = depositAmount;
+    // console.log(totalDeposit.innerText);
+    const previousDepositAmountText = totalDeposit.innerText;
+    const previousDepositAmount = parseFloat(previousDepositAmountText);
+    // console.log(previousDepositAmount);
+
+    const updatedTotalDeposit = depositAmount + previousDepositAmount;
+
+    totalDeposit.innerText = updatedTotalDeposit;
+
+    // update total balance
+    const totalBalance = document.getElementById("total-balance");
+    const previousBalanceAmountText = totalBalance.innerText;
+    const previousBalanceAmount = parseFloat(previousBalanceAmountText);
+    // console.log(previousBalanceAmount);
+
+    const updatedTotalBalance = depositAmount + previousBalanceAmount;
+
+    totalBalance.innerText = updatedTotalBalance;
 
     // clear deposit input field
     depositAmountInput.value = "";

25a768143b5f7d15d01eda39eb2d9b884e1d5dff
TakiKauser <takikauserbadhon@gmail.com>
2021-08-16 05:10:34 +0600

    cleaner code

--- js/functional-banking.js
+++ js/functional-banking.js
@@ -22,8 +22,7 @@ function previousTotalAmount(previousTotalAmountID, inputAmount){
 }
 
 function getPreviousTotalBalanceAmount(){
-     // get previous total balance
-
+    // get previous total balance
     const previousTotalBalance = document.getElementById("total-balance");
     const previousTotalBalanceAmountText = previousTotalBalance.innerText;
     const previousTotalBalanceAmount = parseFloat(previousTotalBalanceAmountText);
@@ -32,13 +31,8 @@ function getPreviousTotalBalanceAmount(){
 }
 
 function updateTotalBalance(inputAmount, isAddition){
-    // get previous total balance
-
+    // get previous total balance 
     const previousTotalBalance = document.getElementById("total-balance");
-    /* 
-    const previousTotalBalanceAmountText = previousTotalBalance.innerText;
-    const previousTotalBalanceAmount = parseFloat(previousTotalBalanceAmountText); 
-    */
     const previousTotalBalanceAmount = getPreviousTotalBalanceAmount();
 
     // update previous total balance
@@ -53,89 +47,24 @@ function updateTotalBalance(inputAmount, isAddition){
 // deposit event handler
 document.getElementById("deposit-btn").addEventListener("click", function(){
     // get depositted amount
-
     const depositAmount = inputtedAmount("deposit-amount-input");
-    /* 
-    const depositAmountInput = document.getElementById("deposit-amount-input");
-    const depositAmountText = depositAmountInput.value;
-    const depositAmount = parseFloat(depositAmountText);
-    */
 
-    // get previous total deposit
-
+    // get and update previous total deposit
     const previousTotalDepositAmount = previousTotalAmount("total-deposit", depositAmount);
-    /* 
-    const previousTotalDeposit = document.getElementById("total-deposit");
-    const previousTotalDepositAmountText = previousTotalDeposit.innerText;
-    const previousTotalDepositAmount = parseFloat(previousTotalDepositAmountText);
 
-    // update total deposit
-    const updatedTotalDepositAmount = depositAmount + previousTotalDepositAmount;
-
-    previousTotalDeposit.innerText = updatedTotalDepositAmount; 
-    */
-     
-    // get previous total balance
-
+    // update total balance
     updateTotalBalance(depositAmount, true);
-    /*
-    const previousTotalBalance = document.getElementById("total-balance");
-    const previousTotalBalanceAmountText = previousTotalBalance.innerText;
-    const previousTotalBalanceAmount = parseFloat(previousTotalBalanceAmountText);
 
-    // update previous total balance
-    const updatedTotalBalanceAmount = previousTotalBalanceAmount + depositAmount;
-
-    previousTotalBalance.innerText = updatedTotalBalanceAmount;
-     */
-
-    // clear input field
-    /*
-    depositAmountInput.value = "";
-    */
 });
 
 // withdraw event handler
 document.getElementById("withdraw-btn").addEventListener("click", function(){
     // get withdraw amount
-
     const withdrawAmount = inputtedAmount("withdraw-amount-input");
-    /*
-    const withdrawAmountInput = document.getElementById("withdraw-amount-input");
-    const withdrawAmountText = withdrawAmountInput.value;
-    const withdrawAmount = parseFloat(withdrawAmountText);
-    */
-
+ 
     // get previous total withdraw
-
     const previousTotalWithdrawAmount = previousTotalAmount("total-withdraw", withdrawAmount);
-    /*
-    const previousTotalWithdraw = document.getElementById("total-withdraw");
-    const previousTotalWithdrawAmountText = previousTotalWithdraw.innerText;
-    const previousTotalWithdrawAmount = parseFloat(previousTotalWithdrawAmountText);
-
-    // update total withdraw
-    updatedTotalWithdrawAmount = previousTotalWithdrawAmount + withdrawAmount;
-
-    previousTotalWithdraw.innerText = updatedTotalWithdrawAmount;
-    */
-
-    // get previous total balance
-    
+ 
+    // update total balance
     updateTotalBalance(withdrawAmount, false);
-    /* 
-    const previousTotalBalance = document.getElementById("total-balance");
-    const previousTotalBalanceAmountText = previousTotalBalance.innerText;
-    const previousTotalBalanceAmount = parseFloat(previousTotalBalanceAmountText);
-
-    // update previous total balance
-    const updatedTotalBalanceAmount = previousTotalBalanceAmount - withdrawAmount;
-
-    previousTotalBalance.innerText = updatedTotalBalanceAmount;
-     */
-
-    // clear input field
-    /*
-    withdrawAmountInput.value = "";
-    */
 });
